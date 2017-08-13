9cad5c9de6baf1bac721a54a1fc686957d1c520d
Simon Gordon <gordonsimon@hotmail.com>
2017-08-13 13:38:28 +1000

    Adding resources at bottom

--- frontend/src/Errata.elm
+++ frontend/src/Errata.elm
@@ -1,18 +1,36 @@
 module Errata exposing (..)
 
-import Html exposing (text, div, p)
-
+import Html exposing (text, div, p, a, ul, li, h2, h3)
+import Html.Attributes exposing (href, target)
 
 stateMachines =
     div []
-        [ p []
-            [ text
-                ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
-                    ++ " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis"
-                    ++ " nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    ++ " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu"
-                    ++ " fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in"
-                    ++ " culpa qui officia deserunt mollit anim id est laborum."
-                )
+    [ 
+        h2 [] [text "Information"],
+        h3 [] [text "About this application"],
+        p []
+        [ text
+            ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+                ++ " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis"
+                ++ " nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                ++ " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu"
+                ++ " fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in"
+                ++ " culpa qui officia deserunt mollit anim id est laborum."
+            )
+        ],
+        h3 [] [text "Resources"],
+        ul []
+        [
+            li []
+            [
+                a
+                [
+                    href "http://blog.markshead.com/869/state-machines-computer-science/",
+                    target "_blank"
+                ]
+                [
+                    text "State Machines - Basics of Comuter Science"
+                ]
             ]
         ]
+    ]
